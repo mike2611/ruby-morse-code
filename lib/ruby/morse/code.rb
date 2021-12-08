7f5718686066677b36536040ee1eb0b776010efd
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-12-08 23:21:54 +0300

    Add decode_char_set method

---
+++ lib/ruby/morse/code.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "code/version"
+
+module Ruby
+  module Morse
+    module Code
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ruby/morse/code/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Ruby
+  module Morse
+    module Code
+      VERSION = "0.1.0"
+    end
+  end
+end
