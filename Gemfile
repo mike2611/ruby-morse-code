7088530bf08b799a5b393a0eaaa4fe9c838f1393
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-12-08 23:57:39 +0300

    Fix linters

--- Gemfile
+++ Gemfile
@@ -1,6 +1,4 @@
 source 'https://rubygems.org'
 
-gemspec
-
 gem 'rake', '~> 13.0'
 gem 'rubocop', '>= 1.0', '< 2.0'
--- lib/ruby/morse/code/version.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Ruby
-  module Morse
-    module Code
-      VERSION = "0.1.0"
-    end
-  end
-end
